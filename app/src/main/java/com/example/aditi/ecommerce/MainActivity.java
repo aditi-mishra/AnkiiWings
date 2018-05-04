9eba6d53ca20cb9f9e786d0f1b0655d49165e2b1
aditi-mishra <aditimishraice@gmail.com>
2018-05-04 15:12:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aditi/ecommerce/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.aditi.ecommerce;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.ankiiwings.com/");
+        webView.setWebViewClient(new WebViewClient());
+    }
+    @Override
+    public  void onBackPressed(){
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else
+            super.onBackPressed();
+    }
+}
